75d6cade15bdff1fa9ebb4a46da55d4d7fb12dcf
Feng Ya <fengya04@126.com>
2017-10-31 23:41:25 +0800

    arrow key

--- src/Grid.js
+++ src/Grid.js
@@ -4,17 +4,68 @@ import PropTypes from 'prop-types';
 import Tile from './Tile';
 
 class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: this.props.cells,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('keydown', event => {
+      const map = {
+        38: 1, // Up
+        39: 2, // Right
+        40: 3, // Down
+        37: 4, // Left
+      };
+      console.log(event.keyCode);
+      if (map[event.keyCode]) {
+        event.preventDefault();
+        this.setState({
+          cells: this.state.cells.map(cell => {
+            if (map[event.keyCode] === 1) {
+              return {
+                ...cell,
+                y: 0,
+              };
+            }
+            if (map[event.keyCode] === 2) {
+              return {
+                ...cell,
+                x: 3,
+              };
+            }
+            if (map[event.keyCode] === 3) {
+              return {
+                ...cell,
+                y: 3,
+              };
+            }
+            if (map[event.keyCode] === 4) {
+              return {
+                ...cell,
+                x: 0,
+              };
+            }
+          }),
+        });
+      }
+    });
+  }
+
   render() {
-    const { cells } = this.props;
+    const { cells } = this.state;
     return (
       <div style={style.grid}>
-        {Array(16)
-          .fill(0)
-          .map((_, index) => (
-            <div key={index} style={style.cell}>
-              <Tile number={cells[index]} />
-            </div>
-          ))}
+        <div style={{ position: 'absolute' }}>
+          {Array(16)
+            .fill(null)
+            .map((_, index) => <div key={index} style={style.cell} />)}
+        </div>
+        <div style={{ position: 'absolute', top: '15px', left: '15px' }}>
+          {cells.map((cell, index) => <Tile key={index} cell={cell} />)}
+        </div>
       </div>
     );
   }
@@ -22,6 +73,7 @@ class Grid extends Component {
 
 const style = {
   grid: {
+    position: 'relative',
     marginTop: '50px',
     padding: '7.5px',
     background: '#bbada0',
@@ -45,7 +97,7 @@ Grid.propTypes = {
 };
 
 Grid.defaultProps = {
-  cells: [2],
+  cells: [{ x: 0, y: 0, value: 2 }],
 };
 
 export default Grid;
--- src/Tile.js
+++ src/Tile.js
@@ -3,80 +3,85 @@ import React, { Component } from 'react';
 import './Tile.css';
 
 class Tile extends Component {
+  getStyle(cell) {
+    const basic = {
+      2: {
+        fontSize: '55px',
+        background: '#eee4da',
+      },
+      4: {
+        fontSize: '55px',
+        background: '#ede0c8',
+      },
+      8: {
+        fontSize: '55px',
+        color: '#f9f6f2',
+        background: '#f2b179',
+      },
+      16: {
+        fontSize: '55px',
+        color: '#f9f6f2',
+        background: '#f59563',
+      },
+      32: {
+        fontSize: '55px',
+        color: '#f9f6f2',
+        background: '#f67c5f',
+      },
+      64: {
+        fontSize: '55px',
+        color: '#f9f6f2',
+        background: '#f65e3b',
+      },
+      128: {
+        fontSize: '45px',
+        color: '#f9f6f2',
+        background: '#edcf72',
+      },
+      256: {
+        fontSize: '45px',
+        color: '#f9f6f2',
+        background: '#edcc61',
+      },
+      512: {
+        fontSize: '45px',
+        color: '#f9f6f2',
+        background: '#edc850',
+      },
+      1024: {
+        fontSize: '35px',
+        color: '#f9f6f2',
+        background: '#edc53f',
+      },
+      2048: {
+        fontSize: '35px',
+        color: '#f9f6f2',
+        background: '#edc22e',
+      },
+      super: {
+        fontSize: '30px',
+        color: '#f9f6f2',
+        background: '#3c3a32',
+      },
+    };
+    return {
+      ...(basic[cell.value] || basic.super),
+      left: `${cell.x * (106.25 + 15)}px`,
+      top: `${cell.y * (106.25 + 15)}px`,
+    };
+  }
+
   render() {
-    const { number } = this.props;
+    const { cell } = this.props;
     return (
-      <div className="tile tile-new" style={styles[number]}>
-        {number}
+      <div
+        className={cell.previousPosition ? 'tile' : 'tile tile-new'}
+        style={this.getStyle(cell)}
+      >
+        {cell.value}
       </div>
     );
   }
 }
-
-const styles = {
-  2: {
-    fontSize: '55px',
-    background: '#eee4da',
-  },
-  4: {
-    fontSize: '55px',
-    background: '#ede0c8',
-  },
-  8: {
-    fontSize: '55px',
-    color: '#f9f6f2',
-    background: '#f2b179',
-  },
-  16: {
-    fontSize: '55px',
-    color: '#f9f6f2',
-    background: '#f59563',
-  },
-  32: {
-    fontSize: '55px',
-    color: '#f9f6f2',
-    background: '#f67c5f',
-  },
-  64: {
-    fontSize: '55px',
-    color: '#f9f6f2',
-    background: '#f65e3b',
-  },
-  128: {
-    fontSize: '45px',
-    color: '#f9f6f2',
-    background: '#edcf72',
-  },
-  256: {
-    fontSize: '45px',
-    color: '#f9f6f2',
-    background: '#edcc61',
-  },
-  512: {
-    fontSize: '45px',
-    color: '#f9f6f2',
-    background: '#edc850',
-  },
-  1024: {
-    fontSize: '35px',
-    color: '#f9f6f2',
-    background: '#edc53f',
-  },
-  2048: {
-    fontSize: '35px',
-    color: '#f9f6f2',
-    background: '#edc22e',
-  },
-  4096: {
-    fontSize: '30px',
-    color: '#f9f6f2',
-    background: '#3c3a32',
-  },
-  8192: {
-    fontSize: '30px',
-    color: '#f9f6f2',
-    background: '#3c3a32',
-  },
-};
 
 export default Tile;
